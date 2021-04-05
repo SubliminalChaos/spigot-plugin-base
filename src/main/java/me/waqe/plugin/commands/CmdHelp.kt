fc84892d90972f64a82653ed986e8b569fc448b2
Rick Pendrick <subliminalchaos@hotmail.com>
2021-04-05 14:33:11 -0500

    Yet even more changes for me.waqe Ad inifintum.. we update!  (-: 2.2

--- src/main/java/me/waqe/plugin/commands/CmdHelp.kt
+++ src/main/java/me/waqe/plugin/commands/CmdHelp.kt
@@ -1,6 +1,7 @@
 package me.waqe.plugin.commands
 
 import me.waqe.plugin.utils.Utils
+import me.waqe.plugin.utils.Utils.cm
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -9,14 +10,12 @@ import org.bukkit.entity.Player
 
 class CmdHelp : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
-        if (!label.equals("help", true))
+        if (label.equals("help", true)) {
+            cm(sender, "&6Usage: /helpme")
             return true
-        if (!sender.hasPermission("App.permission.help"))
-            return true
-        if (sender !is Player)
-            return true
-
-        Utils.color("&7Usage: /command")
+        }
+        //if (!sender.hasPermission("App.permission.help"))
+        //    return true
 
         return true
     }
--- src/main/java/me/waqe/plugin/utils/Utils.kt
+++ src/main/java/me/waqe/plugin/utils/Utils.kt
@@ -1,9 +1,13 @@
 package me.waqe.plugin.utils
 
 import org.bukkit.ChatColor
+import org.bukkit.command.CommandSender
 
 object Utils {
     fun color(message: String) : String {
         return ChatColor.translateAlternateColorCodes('&', message)
+    }
+    fun cm(p: CommandSender, message: String) {
+        p.sendMessage(ChatColor.translateAlternateColorCodes('&', message))
     }
 }
