b6a3f308fec2f3e16c0117b142ca6c84cc6b3c27
Rick Pendrick <subliminalchaos@hotmail.com>
2021-04-05 14:41:44 -0500

    Yet even more changes for me.waqe Ad inifintum.. we update!  (-: 2.5

--- src/main/java/me/waqe/plugin/listeners/ListenerPlayerJoin.kt
+++ src/main/java/me/waqe/plugin/listeners/ListenerPlayerJoin.kt
@@ -1,5 +1,6 @@
 package me.waqe.plugin.listeners
 
+import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -7,6 +8,6 @@ import org.bukkit.event.player.PlayerJoinEvent
 class ListenerPlayerJoin : Listener {
     @EventHandler
     private fun onPlayerJoin(e: PlayerJoinEvent) {
-        e.player.sendMessage("Welcome to the server, ${e.player.displayName}!")
+        e.player.sendMessage("Welcome to the server, ${ChatColor.LIGHT_PURPLE}${e.player.displayName}${ChatColor.WHITE}!")
     }
 }
