e479df1ec7dc4c2bbe39fbe7c0a27a6bb27c332d
Rick Pendrick <subliminalchaos@hotmail.com>
2021-04-05 14:35:21 -0500

    Yet even more changes for me.waqe Ad inifintum.. we update!  (-: 2.3

--- src/main/java/me/waqe/plugin/AppPlugin.kt
+++ src/main/java/me/waqe/plugin/AppPlugin.kt
@@ -1,21 +1,13 @@
 package me.waqe.plugin
 
 import me.waqe.plugin.commands.CmdHelp
-import me.waqe.plugin.configs.ConfigManager
 import me.waqe.plugin.listeners.ListenerPlayerJoin
 import org.bukkit.Bukkit
-import org.bukkit.ChatColor
-import org.bukkit.command.Command
-import org.bukkit.command.CommandSender
-import org.bukkit.entity.Player
-import org.bukkit.event.EventHandler
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerCommandPreprocessEvent
-import org.bukkit.event.player.PlayerJoinEvent
-import org.bukkit.plugin.PluginManager
-import org.bukkit.configuration.file.YamlConfiguration
 import java.io.File
+
+
+
 
 class App : JavaPlugin() {
     companion object {
@@ -23,9 +15,6 @@ class App : JavaPlugin() {
     }
 
     var pluginFolder: File? = null
-
-    var mainCM: ConfigManager? = null
-    var mainConfig: YamlConfiguration? = null
 
     override fun onEnable() {
         instance = this
@@ -42,9 +31,7 @@ class App : JavaPlugin() {
     }
 
     private fun registerConfigs() {
-        mainCM = ConfigManager("config")
-        mainConfig = mainCM!!.getConfig()
-        setupMainConfig()
+        reloadConfig()
     }
 
     private fun registerListeners() {
@@ -54,10 +41,5 @@ class App : JavaPlugin() {
 
     private fun registerCommands() {
         getCommand("help")!!.setExecutor(CmdHelp())
-    }
-
-    private fun setupMainConfig() {
-        mainCM!!.defaultSet("prefix", "[MyPlugin] ")
-        mainCM!!.saveConfig()
     }
 }
